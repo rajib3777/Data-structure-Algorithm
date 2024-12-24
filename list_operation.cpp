68e214a4a116637022cd945a0d277cf81619a702
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2024-12-24 13:44:55 +0600

    Add files via upload

---
+++ list_operation.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public :
+    int val;
+    Node* next;
+    Node* prev;
+
+    Node(int val)
+    {
+        this -> val = val;
+        this -> next = NULL;
+        this -> prev = NULL;
+    }
+};
+
+void insert_it_tail(Node* &head,Node* &tail,int val)
+{
+    Node* newnode = new Node(val);
+
+    if(head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    newnode->prev = tail;
+    tail->next = newnode;
+    tail = newnode;
+}
+
+void print_linked_list(Node* head)
+{
+    Node* temp = head;
+
+    while(temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+
+
+int main()
+{
+    list<int> l;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if(val == -1)
+        {
+            break ;
+        }
+        l.push_back(val);
+    }
+
+    l.reverse();
+
+    l.unique();
+
+    l.sort(greater<int>());
+    
+    auto it = find(l.begin(),l.end(),20);
+    if(it != l.end())
+    {
+        cout << "Found" << endl;
+    }
+    else
+    {
+        cout << "Not found" << endl;
+    }
+
+
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    for(int val : l)
+    {
+        insert_it_tail(head,tail,val);
+    }
+
+
+    print_linked_list(head);
+    
+
+
+    return 0;
+}
