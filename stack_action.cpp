855c3d6bec07ec39326a32ff86ab0b765785f0fb
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-01-07 00:28:35 +0600

    Add files via upload

---
+++ stack_action.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    stack<int> st;
+
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n ; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    cout << "size -> " << endl;
+
+    cout << st.size() << endl;
+
+    if(!st.empty())
+       st.pop();
+
+    
+    cout << "output -> ";
+
+    while(!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << endl;
+
+    cout << "after output size -> " << endl;
+
+    cout << st.size() << endl;
+
+    return 0;
+}
